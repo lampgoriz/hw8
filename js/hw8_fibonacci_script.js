3a98f7ffe8c1520b6bb32d53a4524a99a0a821cd
lampogriz <potolochni@gmail.com>
2019-11-06 17:44:48 +0300

    + fibonacci

--- js/hw8_fibonacci_script.js
+++ js/hw8_fibonacci_script.js
@@ -1,28 +1,16 @@
-//var F0 = 0;
 //var F1 = 1;
-//var n = 25;
-//var F = 0;
-//var Fn_1= 0;
-//var Fn_2 = 0;
-//var temp1 = 0;
-//var temp2 = 0;
-//
-//for(var i = 0; i<25;i++){
-//    
-//    
-//    Fn_1 = F;
-//    F = Fn_1 + Fn_2;
-//    
-//    console.log(F);
-//}
+//var F0 = 0;
 
+var F = 0;
+var Fn_1= 1;
+var Fn_2 = 0;
+console.log(Fn_2);
+console.log(Fn_1);
 
-const fib = n => {
-  let prev = 0, next = 1;
-  for(let i = 0; i < n; i++){
-    let temp = next;
-    next = prev + next;
-    prev = temp;
-  }
-  return prev;
+for(var i = 0; i<25;i++){
+    F = Fn_1 + Fn_2;
+    Fn_2 = Fn_1;
+    Fn_1 = F;
+    console.log(F);
 }
+
